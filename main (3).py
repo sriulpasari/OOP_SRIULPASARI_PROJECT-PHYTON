ae708e785a4c648df81cca3ad557479bd55ef76e
sriulpasari <sariulfa965@gmail.com>
2026-04-02 09:14:07 +0700

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,2 @@
+print("Hello, Dcoder!")
+print ("ORGANIGRAM"); print("ketua kelas"); print("wakil ketua kelas"); print("sekretaris 1"); print("sekretaris 2"); print("bendahara 1"); print("bendahara 2")
