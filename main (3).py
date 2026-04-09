9180448ea84b794bbdf82006032eae4a959f6ac5
sriulpasari <sariulfa965@gmail.com>
2026-04-09 07:21:08 +0700

    Delete main (3).py

--- main (3).py
+++
@@ -1,2 +0,0 @@
-print("Hello, Dcoder!")
-print ("ORGANIGRAM"); print("ketua kelas"); print("wakil ketua kelas"); print("sekretaris 1"); print("sekretaris 2"); print("bendahara 1"); print("bendahara 2")
